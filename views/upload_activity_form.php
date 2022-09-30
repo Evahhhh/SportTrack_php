c4c55d428b74a0d0f1cf50ee4251b518d474816c
Evahhhh <eve-anneoffredi@orange.fr>
2022-09-30 11:32:59 +0200

    update

--- views/upload_activity_form.php
+++ views/upload_activity_form.php
@@ -4,7 +4,7 @@
     <label for="activites">Fichier JSON :</label><br><br>
     <input type="file" id="activites" name="activites" accept="application/JSON"><br><br>
 
-    <input type="submit" value="Envoyer le fichier">
+    <input type="submit" value="Envoyer le fichier"> 
 </form>
             
 <?php include VIEWS_DIR."/footer.html"; ?>
--- views/user_disconnect.php
+++ views/user_disconnect.php
@@ -3,6 +3,6 @@
 include VIEWS_DIR."/header.html";
 
 echo "Déconnexion réussie";
-
+ 
 include VIEWS_DIR."/footer.html";
 ?>
